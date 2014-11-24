b8437d34462f962e9b500b0a7011396a850b975b
irthos <irth03@gmail.com>
2014-11-24 03:40:02 -0800

    loopy controllers, templates, and states

--- build/js/app.js
+++ build/js/app.js
@@ -19,43 +19,53 @@
     .module('angular-skeletor')
     .config(config);
 
-  function config($stateProvider, $urlRouterProvider) {
+  function config($stateProvider, $urlRouterProvider, $controllerProvider) {
     var routes = [
         {name: 'services', type:'page'},
         {name: 'service', type:'item'},
         {name: 'search', type:'query'},
         {name: 'services', type:'admin'}
       ],
-      services = ['data', 'api'];
+      services = ['data', 'api'],
+      makeController = function (name, constructor) {
+        $controllerProvider.register(name, constructor);
+        return (this);
+      };
     console.log(services);
 
     angular.forEach(routes, function (route) {
       var stateName = route.name,
         routeUrl = (function () {
-        if (route.type === 'page') {
-          return '/' + route.name;
-        } else if (route.type === 'item') {
-          return '/' + route.name + '/:' + route.name;
-        } else if (route.type === 'query') {
-          return '/' + route.name + '/:' + route.name;
-        } else if (route.type === 'admin') {
-          stateName = 'admin.' + route.name;
-          return '/' + route.name;
-        }
-      })(),
-        templateUrl = 'main/' + route.type + '.tpl.html';
-      $stateProvider.state(stateName, {
-        url: routeUrl,
-        templateUrl: templateUrl,
-        controller: function ($scope, Api, $state) {
+          if (route.type === 'page') {
+            return '/' + route.name;
+          } else if (route.type === 'item') {
+            return '/' + route.name + '/:' + route.name;
+          } else if (route.type === 'query') {
+            return '/' + route.name + '/:' + route.name;
+          } else if (route.type === 'admin') {
+            stateName = 'admin.' + route.name;
+            return '/' + route.name;
+          }
+        })(),
+        templateUrl = 'main/' + route.type + '.tpl.html',
+        setController = function () {
+        var name = route.name + 'Ctrl',
+        fun = function ($scope, Api, $state) {
           console.log('route.type', route.type);
           console.log('$state', $state);
           $scope.state = $state;
           $scope.Api = Api;
-        }
+        };
+        makeController(name, fun);
+      };
+      setController();
+
+      $stateProvider.state(stateName, {
+        url: routeUrl,
+        templateUrl: templateUrl,
+        controller: route.name + 'Ctrl'
       });
 
-      console.log('route', route);
     });
     $stateProvider.state('admin', {
       url: '/admin',
